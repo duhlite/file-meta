115f73b98eab35fe7cf6ad979258b7eb6728ad07
Glitch (raspy-writer) <none>
2019-03-05 00:32:31 +0000

    🍛🏂 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({storage:storage});
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse', upload.single('upfile'),function(req, res){
+  console.log(req.file.size);
+  res.json({name: req.file.originalname, size: req.file.size});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
